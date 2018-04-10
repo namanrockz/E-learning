df466c3f9b1804596b481fb9f0ec15e6366c0358
namanrockz <chauhan.naman12@gmail.com>
2018-04-10 12:18:27 +0530

    4th

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
-
+const rt: Routes = [
+  {path:'signup',component: SignupComponent },
+  {path:'signin',component: SigninComponent },
+  {path:'home',component: HomeComponent },
+  {path:'courses',component: CoursesComponent },
+  {path:'',redirectTo: '/home',pathMatch:'full' }
+];
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CoursesComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rt)
   ],
   providers: [],
   bootstrap: [AppComponent]
